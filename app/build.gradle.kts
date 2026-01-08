ddb101b5356920e34004c50db35d7db44eb90d20
Rubén Llanes Rioja <rubenllanes47@gmail.com>
2026-01-08 17:25:42 +0100

    Escaneo de prueba activo
    Scanner de QR añadido a falta de probarlo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 }
--- app/src/main/java/ifp/pmdm/aplicacioncitasmedicas/PruebaCamara.kt
+++ app/src/main/java/ifp/pmdm/aplicacioncitasmedicas/PruebaCamara.kt
@@ -3,11 +3,15 @@ package ifp.pmdm.aplicacioncitasmedicas
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.journeyapps.barcodescanner.CaptureActivity
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 import ifp.pmdm.aplicacioncitasmedicas.databinding.ActivityPruebaCamaraBinding
 
 class PruebaCamara : AppCompatActivity() {
@@ -22,6 +26,14 @@ class PruebaCamara : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnCamera.setOnClickListener {
+
+            val options = ScanOptions()
+            options.setPrompt("Scan any QR code of meds")
+            options.setBeepEnabled(true)
+            options.setOrientationLocked(true)
+            options.setCaptureActivity(CaptureActivity::class.java)
+            barcodeLauncher.launch(options)
+
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(intent, 1);
         }
@@ -30,6 +42,14 @@ class PruebaCamara : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private val barcodeLauncher = registerForActivityResult(ScanContract()) { result ->
+        if (result.contents != null) {
+            binding.tvScan.text = "Scanned Ressult : ${result.contents}"
+        } else {
+            Toast.makeText(this, "cancelled", Toast.LENGTH_LONG).show()
         }
     }
 }
